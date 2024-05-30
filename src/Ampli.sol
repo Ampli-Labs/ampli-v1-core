89670e7b178f97ac44bce4bd4b8352d4478ad570
Jeff Huang <jeffishjeff@gmail.com>
2024-05-30 16:17:04 -0700

    position & fiscal operations

--- src/Ampli.sol
+++ src/Ampli.sol
@@ -12,11 +12,13 @@ import {Constants} from "./libraries/Constants.sol";
 import {DeflatorsLibrary} from "./libraries/DeflatorsLibrary.sol";
 import {ExchangeRateLibrary} from "./libraries/ExchangeRateLibrary.sol";
 import {LockLibrary} from "./libraries/LockLibrary.sol";
-import {PositionLibrary, Fungible} from "./libraries/PositionLibrary.sol";
+import {PositionLibrary} from "./libraries/PositionLibrary.sol";
 import {BaseHook, IPoolManager, Hooks, PoolKey, BalanceDelta} from "./modules/externals/BaseHook.sol";
 import {FungibleToken} from "./modules/FungibleToken.sol";
 import {NonFungibleTokenReceiver} from "./modules/NonFungibleTokenReceiver.sol";
 import {RiskConfigs, IRiskGovernor} from "./modules/RiskConfigs.sol";
+import {Fungible, FungibleLibrary} from "./types/Fungible.sol";
+import {NonFungible, NonFungibleLibrary} from "./types/NonFungible.sol";
 
 contract Ampli is IAmpli, BaseHook, FungibleToken, NonFungibleTokenReceiver, RiskConfigs {
     struct InstanceParams {
@@ -36,6 +38,8 @@ contract Ampli is IAmpli, BaseHook, FungibleToken, NonFungibleTokenReceiver, Ris
     using DeflatorsLibrary for DeflatorsLibrary.Deflators;
     using ExchangeRateLibrary for ExchangeRateLibrary.ExchangeRate;
     using PositionLibrary for PositionLibrary.Position;
+    using FungibleLibrary for Fungible;
+    using NonFungibleLibrary for NonFungible;
 
     error NoDelegateCall();
 
@@ -47,7 +51,11 @@ contract Ampli is IAmpli, BaseHook, FungibleToken, NonFungibleTokenReceiver, Ris
     DeflatorsLibrary.Deflators private s_deflators;
     ExchangeRateLibrary.ExchangeRate private s_exchangeRate;
 
+    uint256 private s_deficit;
+    uint256 private s_surplus;
+    uint256 private s_lastPositionId;
     mapping(uint256 => PositionLibrary.Position) private s_positions;
+    mapping(NonFungible => mapping(uint256 => uint256)) private s_nonFungibleItemPositions;
 
     modifier noDelegateCall() {
         if (address(this) != s_self) revert NoDelegateCall();
@@ -97,6 +105,162 @@ contract Ampli is IAmpli, BaseHook, FungibleToken, NonFungibleTokenReceiver, Ris
         delete s_lock.exposedItems;
 
         s_lock.lock();
+    }
+
+    function openPosition(address originator) external noDelegateCall returns (uint256 positionId) {
+        s_positions[(positionId = ++s_lastPositionId)].open(msg.sender, originator);
+
+        emit PositionOpened(positionId, msg.sender, originator);
+    }
+
+    function closePosition(uint256 positionId) external noDelegateCall {
+        if (msg.sender != s_positions[positionId].owner) revert UnauthorizedPositionOperation();
+
+        s_positions[positionId].close();
+
+        emit PositionClosed(positionId);
+    }
+
+    function depositFungible(uint256 positionId, Fungible fungible, uint256 amount) external payable noDelegateCall {
+        if (!s_positions[positionId].exists()) revert PositionDoesNotExist();
+
+        _addFungible(positionId, fungible, amount);
+
+        emit FungibleDeposited(positionId, msg.sender, fungible, amount);
+    }
+
+    function withdrawFungible(uint256 positionId, Fungible fungible, uint256 amount, address recipient)
+        external
+        noDelegateCall
+    {
+        if (msg.sender != s_positions[positionId].owner) revert UnauthorizedPositionOperation();
+        s_lock.expose(positionId);
+
+        _removeFungible(positionId, fungible, amount);
+        fungible.transfer(recipient, amount);
+
+        emit FungibleWithdrawn(positionId, recipient, fungible, amount);
+    }
+
+    function depositNonFungible(uint256 positionId, NonFungible nonFungible, uint256 item) external noDelegateCall {
+        if (!s_positions[positionId].exists()) revert PositionDoesNotExist();
+        if (nonFungible.ownerOf(item) != address(this)) revert NonFungibleItemNotRecieved();
+        if (s_nonFungibleItemPositions[nonFungible][item] != 0) revert NonFungibleItemAlreadyDeposited();
+
+        s_positions[positionId].addNonFungible(nonFungible, item);
+        s_positions[GLOBAL_POSITION_ID].addNonFungible(nonFungible, item);
+        s_nonFungibleItemPositions[nonFungible][item] = positionId;
+
+        emit NonFungibleDeposited(positionId, msg.sender, nonFungible, item);
+    }
+
+    function withdrawNonFungible(uint256 positionId, NonFungible nonFungible, uint256 item, address recipient)
+        external
+        noDelegateCall
+    {
+        if (msg.sender != s_positions[positionId].owner) revert UnauthorizedPositionOperation();
+        if (s_nonFungibleItemPositions[nonFungible][item] != positionId) revert NonFungibleItemNotInPosition();
+        s_lock.expose(positionId);
+
+        s_positions[positionId].removeNonFungible(nonFungible, item);
+        s_positions[GLOBAL_POSITION_ID].removeNonFungible(nonFungible, item);
+        delete s_nonFungibleItemPositions[nonFungible][item];
+        nonFungible.transfer(recipient, item);
+
+        emit NonFungibleWithdrawn(positionId, recipient, nonFungible, item);
+    }
+
+    function borrow(uint256 positionId, uint256 amount, address recipient) external noDelegateCall {
+        if (msg.sender != s_positions[positionId].owner) revert UnauthorizedPositionOperation();
+        s_lock.expose(positionId);
+
+        _mint(recipient, amount);
+        uint256 realAmount = mulDiv(amount, Constants.ONE_UD18, s_deflators.interestAndFeeUD18) + 1;
+        s_positions[GLOBAL_POSITION_ID].realDebt += realAmount; // overflow desired
+        unchecked {
+            s_positions[positionId].realDebt += realAmount;
+        }
+
+        if (recipient == address(this)) {
+            _addFungible(positionId, Fungible.wrap(address(this)), amount);
+        }
+
+        emit Borrow(positionId, recipient, amount, realAmount);
+    }
+
+    function repay(uint256 positionId, uint256 amount) external noDelegateCall {
+        if (msg.sender != s_positions[positionId].owner) revert UnauthorizedPositionOperation();
+
+        _burn(msg.sender, amount);
+        uint256 realAmount = mulDiv(amount, Constants.ONE_UD18, s_deflators.interestAndFeeUD18);
+        s_positions[positionId].realDebt -= realAmount; // underflow desired
+        unchecked {
+            s_positions[GLOBAL_POSITION_ID].realDebt -= realAmount;
+        }
+
+        _removeFungible(positionId, Fungible.wrap(address(this)), amount);
+
+        emit Repay(positionId, amount, realAmount);
+    }
+
+    function liquidate(uint256 positionId, address recipient) external noDelegateCall returns (uint256 shortfall) {
+        if (!s_positions[positionId].exists()) revert PositionDoesNotExist();
+        s_lock.expose(positionId);
+
+        (uint256 value, uint256 marginReq) =
+            s_positions[positionId].appraise(this, Fungible.wrap(address(this)), s_exchangeRate.currentUD18);
+        uint256 debt = s_positions[positionId].nominalDebt(s_deflators.interestAndFeeUD18);
+        if (value >= debt + marginReq) revert PositionNotAtRisk();
+
+        uint256 relief;
+        if (value < debt) {
+            relief = mulDiv(debt, Constants.ONE_UD18, maxDebtRatio()) - value;
+            s_deficit += relief;
+            _mint(address(this), relief);
+            _addFungible(positionId, Fungible.wrap(address(this)), relief);
+        }
+
+        s_positions[positionId].owner = recipient;
+        shortfall = debt + marginReq - (value + relief);
+
+        emit Liquidate(positionId, msg.sender, recipient, relief, shortfall);
+    }
+
+    function exchange(address recipient) external payable noDelegateCall {
+        uint256 amount = mulDiv(msg.value, s_exchangeRate.currentUD18, Constants.ONE_UD18);
+
+        s_surplus += msg.value;
+        _mint(recipient, amount);
+
+        emit Exchange(msg.sender, recipient, msg.value, amount);
+    }
+
+    function settle() external noDelegateCall {
+        uint256 deficit = s_deficit;
+
+        if (deficit > 0) {
+            uint256 balance = balanceOf(msg.sender);
+            uint256 amount = balance >= deficit ? deficit : balance;
+
+            s_deficit -= amount;
+            _burn(msg.sender, amount);
+
+            emit Settle(msg.sender, amount);
+        }
+    }
+
+    function collect(address recipient, uint256 amount) external noDelegateCall riskGovernorOnly {
+        if (s_deficit == 0) {
+            uint256 surplus = s_surplus;
+
+            amount = amount == 0 ? surplus : amount;
+            if (surplus < amount) revert InsufficientSurplus();
+
+            s_surplus -= amount;
+            FungibleLibrary.NATIVE.transfer(recipient, amount);
+
+            emit Collect(recipient, amount);
+        }
     }
 
     function beforeAddLiquidity(
@@ -176,6 +340,24 @@ contract Ampli is IAmpli, BaseHook, FungibleToken, NonFungibleTokenReceiver, Ris
         }
 
         return (this.afterSwap.selector, 0);
+    }
+
+    function _addFungible(uint256 positionId, Fungible fungible, uint256 amount) private {
+        PositionLibrary.Position storage s_globalPosition = s_positions[GLOBAL_POSITION_ID];
+        uint256 received = fungible.balanceOf(address(this)) - s_globalPosition.fungibleAssets[fungible].balance
+            - (fungible == FungibleLibrary.NATIVE ? s_surplus : 0);
+        if (received < amount) revert FungibleAmountNotRecieved();
+
+        s_positions[positionId].addFungible(fungible, amount);
+        s_globalPosition.addFungible(fungible, amount);
+    }
+
+    function _removeFungible(uint256 positionId, Fungible fungible, uint256 amount) private {
+        PositionLibrary.Position storage s_position = s_positions[positionId];
+        if (s_position.fungibleAssets[fungible].balance < amount) revert FungibleBalanceInsufficient();
+
+        s_position.removeFungible(fungible, amount);
+        s_positions[GLOBAL_POSITION_ID].removeFungible(fungible, amount);
     }
 
     function _disburseInterest() private {
--- src/interfaces/IAmpli.sol
+++ src/interfaces/IAmpli.sol
@@ -3,10 +3,65 @@ pragma solidity >=0.8.0;
 
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {IERC721TokenReceiver} from "forge-std/interfaces/IERC721.sol";
+import {Fungible} from "../types/Fungible.sol";
+import {NonFungible} from "../types/NonFungible.sol";
 import {IRiskConfigs} from "./IRiskConfigs.sol";
 
 interface IAmpli is IERC20, IERC721TokenReceiver, IRiskConfigs {
+    event PositionOpened(uint256 indexed positionId, address indexed owner, address indexed originator);
+    event PositionClosed(uint256 indexed positionId);
+    event FungibleDeposited(
+        uint256 indexed positionId, address indexed operator, Fungible indexed fungible, uint256 amount
+    );
+    event FungibleWithdrawn(
+        uint256 indexed positionId, address indexed recipient, Fungible indexed fungible, uint256 amount
+    );
+    event NonFungibleDeposited(
+        uint256 indexed positionId, address indexed operator, NonFungible indexed nonFungible, uint256 item
+    );
+    event NonFungibleWithdrawn(
+        uint256 indexed positionId, address indexed recipient, NonFungible indexed nonFungible, uint256 item
+    );
+    event Borrow(uint256 indexed positionId, address indexed recipient, uint256 nominalAmount, uint256 realAmount);
+    event Repay(uint256 indexed positionId, uint256 nominalAmount, uint256 realAmount);
+    event Liquidate(
+        uint256 indexed positionId,
+        address indexed operator,
+        address indexed recipient,
+        uint256 relief,
+        uint256 shortfall
+    );
+    event Exchange(address indexed operator, address indexed recipient, uint256 nativeAmount, uint256 amount);
+    event Settle(address indexed operator, uint256 amount);
+    event Collect(address indexed recipient, uint256 nativeAmount);
+
+    error UnauthorizedPositionOperation();
+    error PositionDoesNotExist();
+    error PositionNotAtRisk();
     error PositionAtRisk(uint256 positionId);
+    error FungibleAmountNotRecieved();
+    error FungibleBalanceInsufficient();
+    error NonFungibleItemNotRecieved();
+    error NonFungibleItemAlreadyDeposited();
+    error NonFungibleItemNotInPosition();
+    error InsufficientSurplus();
 
     function unlock(bytes calldata callbackData) external returns (bytes memory callbackResult);
+
+    function openPosition(address originator) external returns (uint256 positionId);
+    function closePosition(uint256 positionId) external;
+
+    function depositFungible(uint256 positionId, Fungible fungible, uint256 amount) external payable;
+    function withdrawFungible(uint256 positionId, Fungible fungible, uint256 amount, address recipient) external;
+    function depositNonFungible(uint256 positionId, NonFungible nonFungible, uint256 item) external;
+    function withdrawNonFungible(uint256 positionId, NonFungible nonFungible, uint256 item, address recipient)
+        external;
+
+    function borrow(uint256 positionId, uint256 amount, address recipient) external;
+    function repay(uint256 positionId, uint256 amount) external;
+    function liquidate(uint256 positionId, address recipient) external returns (uint256 shortfall);
+
+    function exchange(address recipient) external payable;
+    function settle() external;
+    function collect(address recipient, uint256 amount) external;
 }
